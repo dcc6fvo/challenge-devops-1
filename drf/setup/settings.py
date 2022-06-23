e30802fa3dc50ab60affe463eb68e73670101682
felipe.volpato <fvolpato@gmail.com>
2022-06-23 16:47:03 -0300

    Incluindo opcao com banco Mysql

--- drf/setup/settings.py
+++ drf/setup/settings.py
@@ -67,8 +67,15 @@ WSGI_APPLICATION = 'setup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.path.join(BASE_DIR, 'db.mysql'),
+        'USER': 'root',  
+        'PASSWORD': 'supersecret',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }
     }
 }
 
